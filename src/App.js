d35769f003158092ae77abd3496f51839c193c8b
SergioooAGS <serginchis@gmail.com>
2023-12-06 22:51:04 -0600

    subida 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Prueba de sergio <code>Wachando</code> Equipo tnt
+        </p>
+        <p>
+          El Monobaldo
         </p>
         <a
           className="App-link"
